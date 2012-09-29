d18f05229a7c7193119bd0a3d183adffa6a5b3dd
Leo <lr58@duke.edu>
2012-09-29 15:36:10 -0400

    added navigation to pager button

---
+++ MDAssistant/PagerViewController.m
@@ -0,0 +1,67 @@
+//
+//  PagerViewController.m
+//  MDAssistant
+//
+//  Created by Leo on 9/29/12.
+//  Copyright (c) 2012 guest user. All rights reserved.
+//
+
+#import "PagerViewController.h"
+
+@interface PagerViewController ()
+
+@end
+
+@implementation PagerViewController
+
+{
+    NSArray *options;
+
+}
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+  options = [NSArray arrayWithObjects:@"Cross Cover", @"Signout to OR", @"Sign back to on page", @"Refer to Cell Phone", nil];
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+- (void)viewDidUnload {
+    [self setPagerViewController:nil];
+    [super viewDidUnload];
+}
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    return [options count];
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    static NSString *simpleTableIdentifier = @"CCcell";
+    
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:simpleTableIdentifier];
+    
+    if (cell == nil) {
+        cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:simpleTableIdentifier];
+    }
+    
+    cell.textLabel.text = [options objectAtIndex:indexPath.row];
+    return cell;
+}
+
+@end
---
+++ PagerViewController.h
@@ -0,0 +1,13 @@
+//
+//  PagerViewController.h
+//  MDAssistant
+//
+//  Created by Leo on 9/29/12.
+//  Copyright (c) 2012 guest user. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PagerViewController : UIViewController
+
+@end
