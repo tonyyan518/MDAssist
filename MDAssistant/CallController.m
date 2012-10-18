01e3dfb51e031b8e5b03aa258f63384daa4a8bc1
Leo <lr58@duke.edu>
2012-10-18 16:34:46 -0400

    a

--- MDAssistant/CallController.m
+++ MDAssistant/CallController.m
@@ -34,7 +34,7 @@
     [super viewDidUnload];
     // Release any retained subviews of the main view.
     // e.g. self.myOutlet = nil;
-}
+}   
 
 - (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
 {
@@ -45,7 +45,7 @@
     NSUserDefaults *defaults = [NSUserDefaults standardUserDefaults];
     NSString *pagerNum = [defaults objectForKey:@"savedPager"];
     NSString *uniqueID = [defaults objectForKey:@"savedID"];
-    NSString *backOnPage = [NSString stringWithFormat:@"tel:919970%@,,*#,%@,12", pagerNum, uniqueID];
+    NSString *backOnPage = [NSString stringWithFormat:@"te   l:919970%@,,*#,%@,12", pagerNum, uniqueID];
     NSLog(backOnPage);
     [[UIApplication sharedApplication] openURL:[NSURL URLWithString:backOnPage]];
 }
