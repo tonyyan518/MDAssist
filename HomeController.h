6f24c4d82733c08e10f2d595281c7fbf71cb2a05
tyy <tonyyan518@gmail.com>
2012-12-09 22:40:05 -0500

    Security Code up and running. 'test' is the current code

--- HomeController.h
+++ HomeController.h
@@ -2,15 +2,11 @@
 //  HomeController.h
 //  MDAssistant
 //
-//  Created by guest user on 10/18/12.
-//  Copyright (c) 2012 guest user. All rights reserved.
+//  Created by Tony Yan
 //
 
 #import <UIKit/UIKit.h>
 
 @interface HomeController : UIViewController
-
-- (IBAction)openHandbook:(id)sender;
-- (IBAction)openDictation:(id)sender;
 
 @end
--- HomeController.m
+++ HomeController.m
@@ -2,39 +2,40 @@
 //  HomeController.m
 //  MDAssistant
 //
-//  Created by guest user on 10/18/12.
-//  Copyright (c) 2012 guest user. All rights reserved.
+//  Created by Tony Yan
 //
 
 #import "HomeController.h"
 
 #define PAGER_SEGUE @"pagerSegue"
+#define CONTACT_SEGUE @"contactSegue"
+#define CALL_SEGUE @"callSegue"
+#define DICTATION_SEGUE @"dictationSegue"
+#define HANDBOOK_SEGUE @"handbookSegue"
 
 @interface HomeController ()
 
 @end
 
-@implementation HomeController
+@implementation HomeController {
+    NSString *securityKey;
+}
 
 - (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
 {
     self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
-    if (self) {
-        // Custom initialization
-    }
     return self;
 }
 
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-	// Do any additional setup after loading the view.
+    securityKey = @"test";
 }
 
 - (void)viewDidUnload
 {
     [super viewDidUnload];
-    // Release any retained subviews of the main view.
 }
 
 - (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
@@ -48,22 +49,76 @@
     NSString *uniqueID = [defaults objectForKey:@"savedID"];
     NSString *pagerNum = [defaults objectForKey:@"savedPager"];
     NSString *cellNum = [defaults objectForKey:@"savedCell"];
-    if(uniqueID != NULL && pagerNum != NULL && cellNum != NULL) {
-        [self performSegueWithIdentifier:PAGER_SEGUE sender:self];
+    NSString *securityCode = [defaults objectForKey:@"securityCode"];
+    if ([securityCode isEqual:securityKey]) {
+        if(uniqueID != NULL && pagerNum != NULL && cellNum != NULL) {
+            [self performSegueWithIdentifier:PAGER_SEGUE sender:self];
+        }
+        else {
+            UIAlertView *alert = [[UIAlertView alloc] initWithTitle:nil message:@"Please fill out Settings before using the Pager Assistant." delegate:self cancelButtonTitle:@"OK" otherButtonTitles:nil, nil];
+            [alert show];
+        }
     }
     else {
-        UIAlertView *alert = [[UIAlertView alloc] initWithTitle:nil message:@"Please fill out Settings before using the Pager Assistant" delegate:self cancelButtonTitle:@"OK" otherButtonTitles:nil, nil];
-        [alert show];    }
+        UIAlertView *alert = [[UIAlertView alloc] initWithTitle:nil message:@"Incorrect Security Code. Please check Settings." delegate:self cancelButtonTitle:@"OK" otherButtonTitles:nil, nil];
+        [alert show];
+    }
+}
+
+- (IBAction)contactButtonPressed:(id)sender
+{
+    NSUserDefaults *defaults = [NSUserDefaults standardUserDefaults];
+    NSString *securityCode = [defaults objectForKey:@"securityCode"];
+    if ([securityCode isEqual:securityKey]) {
+        [self performSegueWithIdentifier:CONTACT_SEGUE sender:self];
+    }
+    else {
+        UIAlertView *alert = [[UIAlertView alloc] initWithTitle:nil message:@"Incorrect Security Code. Please check Settings." delegate:self cancelButtonTitle:@"OK" otherButtonTitles:nil, nil];
+        [alert show];
+    }
+}
+
+- (IBAction)dictationButtonPressed:(id)sender
+{
+    NSUserDefaults *defaults = [NSUserDefaults standardUserDefaults];
+    NSString *securityCode = [defaults objectForKey:@"securityCode"];
+    if ([securityCode isEqual:securityKey]) {
+        [self performSegueWithIdentifier:DICTATION_SEGUE sender:self];
+    }
+    else {
+        UIAlertView *alert = [[UIAlertView alloc] initWithTitle:nil message:@"Incorrect Security Code. Please check Settings." delegate:self cancelButtonTitle:@"OK" otherButtonTitles:nil, nil];
+        [alert show];
+    }
+}
+
+- (IBAction)handbookButtonPressed:(id)sender
+{
+    NSUserDefaults *defaults = [NSUserDefaults standardUserDefaults];
+    NSString *securityCode = [defaults objectForKey:@"securityCode"];
+    if ([securityCode isEqual:securityKey]) {
+        [self performSegueWithIdentifier:HANDBOOK_SEGUE sender:self];
+    }
+    else {
+        UIAlertView *alert = [[UIAlertView alloc] initWithTitle:nil message:@"Incorrect Security Code. Please check Settings." delegate:self cancelButtonTitle:@"OK" otherButtonTitles:nil, nil];
+        [alert show];
+    }
+}
+
+- (IBAction)callButtonPressed:(id)sender
+{
+    NSUserDefaults *defaults = [NSUserDefaults standardUserDefaults];
+    NSString *securityCode = [defaults objectForKey:@"securityCode"];
+    if ([securityCode isEqual:securityKey]) {
+        [self performSegueWithIdentifier:CALL_SEGUE sender:self];
+    }
+    else {
+        UIAlertView *alert = [[UIAlertView alloc] initWithTitle:nil message:@"Incorrect Security Code. Please check Settings." delegate:self cancelButtonTitle:@"OK" otherButtonTitles:nil, nil];
+        [alert show];
+    }
 }
 
 - (IBAction)openHandbook:(id)sender {
     NSString *fileName = @"Resident Handbook Final3";
-    NSUserDefaults *defaults = [NSUserDefaults standardUserDefaults];
-    [defaults setObject:fileName forKey:@"fileName"];
-}
-
-- (IBAction)openDictation:(id)sender {
-    NSString *fileName = @"Dictation Templates";
     NSUserDefaults *defaults = [NSUserDefaults standardUserDefaults];
     [defaults setObject:fileName forKey:@"fileName"];
 }
