a924a07d3047b9885f96f27c6bb8f875c4641d74
tyy <tonyyan518@gmail.com>
2012-12-16 22:42:10 -0500

    XMLParser commented

--- MDAssistant/XMLParser.m
+++ MDAssistant/XMLParser.m
@@ -12,10 +12,13 @@
 @synthesize entry, entries;
 
 - (XMLParser *) initXMLParser {
+    //initialize with an array of all the parsed entries
     entries = [[NSMutableArray alloc] init];
     return [super init];
 }
 
+//whenever the parser comes across an "entry", initialize a new 'Entry'
+//otherwise, clear the currentElementValue
 - (void)parser:(NSXMLParser *)parser didStartElement:(NSString *)elementName
   namespaceURI:(NSString *)namespaceURI qualifiedName:(NSString *)qualifiedName
     attributes:(NSDictionary *)attributeDict {
@@ -26,6 +29,7 @@
         currentElementValue = nil;
 }
 
+//populate currentElementValue with the value of the current element
 - (void)parser:(NSXMLParser *)parser foundCharacters:(NSString *)string {
     if(!currentElementValue) {
         currentElementValue = [[NSMutableString alloc] initWithString:string];
@@ -37,17 +41,19 @@
 
 - (void)parser:(NSXMLParser *)parser didEndElement:(NSString *)elementName
   namespaceURI:(NSString *)namespaceURI qualifiedName:(NSString *)qName {
-    
+    //end of the file
     if([elementName isEqualToString:@"entries"])
         return;
-    
+    //end of an entry, so add it to the array of all entries
     if([elementName isEqualToString:@"entry"]) {
         [entries addObject:entry];
         entry = nil;
     }
+    //a part of an entry; add it to the entry
     else {
         [entry setValue:currentElementValue forKey:elementName];
     }
+    //clear the currentElementValue
     currentElementValue = nil;
 }
 
